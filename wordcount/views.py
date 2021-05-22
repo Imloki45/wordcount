d7ac8b35a9bb979ba7d885fd08064d8516c75084
Imloki45 <klokesh1999@gmail.com>
2021-05-22 20:47:51 +0530

    add form post method

--- wordcount/views.py
+++ wordcount/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render
 def home(request):
   return render(request,'home.html')
 def count(request):
-  dis=request.GET['text']
+  if request.method == "POST":
+    dis=request.POST["text"]
   l=len(dis.split())
   return render(request,'co.html',{"text":dis,"len":l})
 def about(request):
   return render(request,'about.html')
+    
