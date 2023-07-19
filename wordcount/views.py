a68cd3d0805e6ea4cff53925391c9fdc7706cd07
Imloki45 <klokesh1999@gmail.com>
2023-07-19 15:19:05 +0530

    Added total characters

--- wordcount/views.py
+++ wordcount/views.py
@@ -1,12 +1,19 @@
+import json,os
 from django.http import HttpResponse
 from django.shortcuts import render
+
 def home(request):
   return render(request,'home.html')
 def count(request):
   if request.method == "POST":
     dis=request.POST["text"]
   l=len(dis.split())
-  return render(request,'co.html',{"text":dis,"len":l})
+  charscount=len(dis)
+  return render(request,'co.html',{"text":dis,"len":l,"charCount":charscount})
 def about(request):
   return render(request,'about.html')
-    
+def displayBooks(request):
+  # print(os.getcwd())
+  # print(open('wordcount\\books.json')) 
+  return render(request,'displayBooks.html',{"books":json.load(open('wordcount\\books.json'))})
+
